beccf03bb739497a3dc94e48af0db59650233c5e
adfhsjt <ahmadriski207@gmail.com>
2026-04-30 17:17:40 +0700

    fix: perbaiki error konfigurasi devIndicators Next.js

--- clothesline-web/next.config.ts
+++ clothesline-web/next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
+      }
+    ],
+  }
 };
 
 export default nextConfig;
